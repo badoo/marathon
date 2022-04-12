327d646940c131fb03d55c9f81cbbd2bda4b378a
Ivan Dyatlov <ivan.dyatlov@corp.badoo.com>
2022-04-12 10:39:52 +0100

    Don't mark incomplete tests as flaky when there are infra issues (e.g. device offline)

--- core/src/main/kotlin/com/malinskiy/marathon/report/summary/TestSummary.kt
+++ core/src/main/kotlin/com/malinskiy/marathon/report/summary/TestSummary.kt
@@ -12,7 +12,11 @@ data class TestSummary(
 
     val isFlaky: Boolean by lazy {
         val hasSuccessResult = results.any { it.status == TestStatus.PASSED }
-        val hasFailedResult = results.any { it.status == TestStatus.FAILURE || it.status == TestStatus.INCOMPLETE }
+        val hasFailedResult = results.any {
+            // stack trace will be null in case of device infra issues, e.g. device offline etc.
+            // we will still report these tests as incomplete for stats, but we shouldn't mark them as flaky
+            it.status == TestStatus.FAILURE || (it.status == TestStatus.INCOMPLETE && it.stacktrace != null)
+        }
         hasSuccessResult && hasFailedResult
     }
 
