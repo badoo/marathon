c220373bca14d4e35f6deb129e21a025ba233fb0
Sergey Chelombitko <sergey.chelombitko@team.bumble.com>
2023-05-17 16:29:36 +0100

    Gradle 8.2.1 and Kotlin 1.8.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,14 +34,13 @@ allprojects {
     }
 
     extensions.findByType<JavaPluginExtension>()?.run {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     project.tasks.withType<KotlinCompile>().configureEach {
         kotlinOptions {
-            apiVersion = "1.6"
-            jvmTarget = "11"
+            jvmTarget = "17"
             freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
         }
     }
