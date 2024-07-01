6f683cbf28cb98b54f7c2947dd4c73d6635b66bc
Sergey Chelombitko <sergey.chelombitko@team.bumble.com>
2024-07-01 16:37:34 +0100

    Revamp samples

--- sample/android-library/buildSrc/src/main/kotlin/Versions.kt
+++
@@ -1,9 +0,0 @@
-object Versions {
-    val androidxTest = "1.3.0"
-    val androidxTestJUnit = "1.1.2"
-}
-
-object TestLibraries {
-    val androidxTestRunner = "androidx.test:runner:${Versions.androidxTest}"
-    val androidxTestJUnit = "androidx.test.ext:junit:${Versions.androidxTestJUnit}"
-}
