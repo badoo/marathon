7fa3a0a601ad6ad1858daf8ee6380fd4f0ca8e85
Ivan Dyatlov <ivan.dyatlov@corp.badoo.com>
2024-07-05 15:49:28 +0100

    Use single instance of CachedFileHasher instead of separate one

--- vendor/vendor-android/base/src/main/kotlin/com/malinskiy/marathon/android/di/Modules.kt
+++ vendor/vendor-android/base/src/main/kotlin/com/malinskiy/marathon/android/di/Modules.kt
@@ -11,8 +11,6 @@ import com.malinskiy.marathon.android.executor.logcat.parse.LogcatEventsListener
 import com.malinskiy.marathon.cache.test.key.ComponentCacheKeyProvider
 import com.malinskiy.marathon.execution.ComponentInfoExtractor
 import com.malinskiy.marathon.execution.TestParser
-import com.malinskiy.marathon.io.CachedFileHasher
-import com.malinskiy.marathon.io.Md5FileHasher
 import com.malinskiy.marathon.log.MarathonLogConfigurator
 import com.malinskiy.marathon.report.logs.LogsProvider
 import org.koin.dsl.module
@@ -20,7 +18,7 @@ import org.koin.dsl.module
 val androidModule = module {
     single<TestParser?> { AndroidTestParser() }
     single<ComponentInfoExtractor?> { AndroidComponentInfoExtractor() }
-    single<ComponentCacheKeyProvider?> { AndroidComponentCacheKeyProvider(CachedFileHasher(Md5FileHasher())) }
+    single<ComponentCacheKeyProvider?> { AndroidComponentCacheKeyProvider(get()) }
     single<LogcatCollector?> { LogcatCollector() }
     single<LogcatEventsListener?> { get<LogcatCollector>() }
     single<LogsProvider?> { get<LogcatCollector>() }
