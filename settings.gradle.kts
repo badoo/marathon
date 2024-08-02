264142447f088bbdb2791f9150b58e20db4d06c3
Sergey Chelombitko <sergey.chelombitko@team.bumble.com>
2024-08-02 15:57:52 +0100

    Gradle 8.9

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
         mavenCentral()
     }
     plugins {
-        id("org.jetbrains.kotlin.jvm") version "1.9.22"
+        id("org.jetbrains.kotlin.jvm") version "1.9.23"
         id("org.jetbrains.dokka") version "0.9.17"
         id("io.gitlab.arturbosch.detekt") version "1.0.1"
     }
