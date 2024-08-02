325859c5b6f3f1ab28987e69bccd958743bd2d50
Sergey Chelombitko <sergey.chelombitko@team.bumble.com>
2024-08-02 15:27:54 +0100

    Convert sample tests to Kotlin

---
+++ sample/library/src/androidTest/java/com/library/LibraryTest.kt
@@ -0,0 +1,34 @@
+package com.library
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LibraryTest {
+    @Test
+    fun test1() {
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun test2() {
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun test3() {
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun test4() {
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun test5Failed() {
+        Thread.sleep(1000)
+        throw AssertionError()
+    }
+}
