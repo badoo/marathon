e979c88c682ed0bb59a9bbe0d3be85708738c7b8
Sergey Chelombitko <sergey.chelombitko@team.bumble.com>
2024-08-03 12:14:17 +0100

    Remove publishing to GitHub

--- buildSrc/src/main/kotlin/Deployment.kt
+++ buildSrc/src/main/kotlin/Deployment.kt
@@ -4,20 +4,12 @@ import org.gradle.api.publish.maven.MavenPom
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.create
-import org.gradle.kotlin.dsl.extra
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.provideDelegate
-import org.gradle.kotlin.dsl.the
-import org.gradle.plugins.signing.SigningExtension
-import java.net.URI
 
 object Deployment {
-    val githubUser = System.getenv("GITHUB_MAVEN_USERNAME")
-    val githubPassword = System.getenv("GITHUB_MAVEN_PASSWORD")
     var releaseMode: String? = null
     var versionSuffix: String? = null
-
-    val githubDeployUrl = "https://maven.pkg.github.com/badoo"
 
     fun initialize(project: Project) {
         val releaseMode: String? by project
@@ -30,7 +22,6 @@ object Deployment {
         Deployment.versionSuffix = versionSuffix
 
         initializePublishing(project)
-        initializeSigning(project)
     }
 
     private fun initializePublishing(project: Project) {
@@ -45,32 +36,6 @@ object Deployment {
                     from(project.components["java"])
                 }
             }
-            repositories {
-                maven {
-                    name = "GitHub"
-                    credentials {
-                        username = Deployment.githubUser
-                        password = Deployment.githubPassword
-                    }
-                    url = URI.create(Deployment.githubDeployUrl)
-                }
-            }
-        }
-    }
-
-    private fun initializeSigning(project: Project) {
-        val passphrase = System.getenv("GPG_PASSPHRASE")
-        passphrase?.let {
-            project.plugins.apply("signing")
-
-            val publishing = project.the(PublishingExtension::class)
-            project.configure<SigningExtension> {
-                sign(publishing.publications.getByName("default"))
-            }
-
-            project.extra.set("signing.keyId", "1131CBA5")
-            project.extra.set("signing.password", passphrase)
-            project.extra.set("signing.secretKeyRingFile", "${project.rootProject.rootDir}/.buildsystem/secring.gpg")
         }
     }
 
